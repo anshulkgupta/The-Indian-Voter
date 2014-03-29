3cb6f10bd995fc2a7d53645949f91aa3d2ebe9d6
Anshul Gupta <anshul.dlf@gmail.com>
2014-03-29 16:52:49 -0400

    uploaded quite a bit

---
+++ Code/download_data.py
@@ -0,0 +1,115 @@
+import twitter
+import json
+
+
+# XXX: Go to http://dev.twitter.com/apps/new to create an app and get values
+# for these credentials, which you'll need to provide in place of these
+# empty string values that are defined as placeholders.
+# See https://dev.twitter.com/docs/auth/oauth for more information 
+# on Twitter's OAuth implementation.
+
+CONSUMER_KEY = 'fTVGqEOwEtS5jCSV2ZA'
+CONSUMER_SECRET = '5epDQkOVCUMhSc2f7JK4Gw3uJ63hPEtMIdAAzDOptE'
+OAUTH_TOKEN = '604597818-dkfIvsfKdukbqe1Ay1KvZq4k0NcrEv5hYw4qj86B'
+OAUTH_TOKEN_SECRET = '4MLU0w8kCoGkCmtw8sOtNspqBOfO33Wcdg5mkq73JGt51'
+
+auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+                           CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter_api = twitter.Twitter(auth=auth)
+
+
+# Set this variable to a trending topic, 
+# or anything else for that matter. The example query below
+# was a trending topic when this content was being developed
+# and is used throughout the remainder of this chapter.
+
+q = '#AAP' 
+
+count = 10000
+
+# See https://dev.twitter.com/docs/api/1.1/get/search/tweets
+
+search_results = twitter_api.search.tweets(q=q, count=count)
+
+statuses = search_results['statuses']
+
+
+# Iterate through 5 more batches of results by following the cursor
+
+for _ in range(5):
+    print "Length of statuses", len(statuses)
+    try:
+        next_results = search_results['search_metadata']['next_results']
+    except KeyError, e: # No more results when next_results doesn't exist
+        break
+        
+    # Create a dictionary from next_results, which has the following form:
+    # ?max_id=313519052523986943&q=NCAA&include_entities=1
+    kwargs = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
+    
+    search_results = twitter_api.search.tweets(**kwargs)
+    statuses += search_results['statuses']
+
+# Show one sample search result by slicing the list...
+print json.dumps(statuses[0], indent=1)
+
+
+# Write json dumps to a temporary file
+my_file = open("sample_output.txt", 'w')
+my_file.write(json.dumps(statuses[0], indent=1))
+my_file.close()
+print "\nFile write succesful!"
+
+
+#
+# Extracting text, screen names, and hashtags from tweets
+# 
+status_texts = [ status['text'] 
+                 for status in statuses ]
+
+screen_names = [ user_mention['screen_name'] 
+                 for status in statuses
+                     for user_mention in status['entities']['user_mentions'] ]
+
+hashtags = [ hashtag['text'] 
+             for status in statuses
+                 for hashtag in status['entities']['hashtags'] ]
+
+# Compute a collection of all words from all tweets
+words = [ w 
+          for t in status_texts 
+              for w in t.split() ]
+
+# Explore the first 5 items for each...
+
+print json.dumps(status_texts[0:5], indent=1)
+print json.dumps(screen_names[0:5], indent=1) 
+print json.dumps(hashtags[0:5], indent=1)
+print json.dumps(words[0:5], indent=1)
+
+
+#
+# Creating a basic frequency distribution from the words in tweets
+#
+from collections import Counter
+
+for item in [words, screen_names, hashtags]:
+    c = Counter(item)
+    print c.most_common()[:10] # top 10
+    print
+
+#
+# Using prettytable to display tuples in a nice tabular format
+#
+
+from prettytable import PrettyTable
+
+for label, data in (('Word', words), 
+                    ('Screen Name', screen_names), 
+                    ('Hashtag', hashtags)):
+    pt = PrettyTable(field_names=[label, 'Count']) 
+    c = Counter(data)
+    [ pt.add_row(kv) for kv in c.most_common()[:10] ]
+    pt.align[label], pt.align['Count'] = 'l', 'r' # Set column alignment
+    print pt
