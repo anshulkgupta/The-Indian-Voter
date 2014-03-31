33511aabf5f4a44dd8486d70a498d77225fcdfd1
Anshul Gupta <anshul.dlf@gmail.com>
2014-03-31 02:04:37 -0400

    Update credentials.py

--- Code/twitter_stream_mining/credentials.py
+++ Code/twitter_stream_mining/credentials.py
@@ -3,8 +3,8 @@
 access_token = '604597818-dkfIvsfKdukbqe1Ay1KvZq4k0NcrEv5hYw4qj86B'
 access_token_secret = '4MLU0w8kCoGkCmtw8sOtNspqBOfO33Wcdg5mkq73JGt51'
 
-consumer_key = 'fTVGqEOwEtS5jCSV2ZA'
-consumer_secret = '5epDQkOVCUMhSc2f7JK4Gw3uJ63hPEtMIdAAzDOptE'
+consumer_key = ''
+consumer_secret = ''
 
 # Geonames
 # http://www.geonames.org/export/web-services.html
