242ff927d32be6e8b81c10016f9109de28659b9a
Anshul Gupta <anshul.dlf@gmail.com>
2014-04-22 00:26:18 -0400

    Update scrape.py

--- Code/scrape.py
+++ Code/scrape.py
@@ -1,11 +1,6 @@
 import sys
 import tweepy
 
-access_key = '604597818-dkfIvsfKdukbqe1Ay1KvZq4k0NcrEv5hYw4qj86B'
-access_secret = '4MLU0w8kCoGkCmtw8sOtNspqBOfO33Wcdg5mkq73JGt51'
-
-consumer_key = 'fTVGqEOwEtS5jCSV2ZA'
-consumer_secret = '5epDQkOVCUMhSc2f7JK4Gw3uJ63hPEtMIdAAzDOptE'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
